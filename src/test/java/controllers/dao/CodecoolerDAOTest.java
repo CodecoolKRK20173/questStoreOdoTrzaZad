9651185b896cd20a1ddb07ca38d07a135a2de61e
Paprok <rafal.becla@gmail.com>
2018-11-23 00:11:00 +0100

    created getCodecoolerModelReturnsValidModel test

--- src/test/java/controllers/dao/CodecoolerDAOTest.java
+++ src/test/java/controllers/dao/CodecoolerDAOTest.java
@@ -1,5 +1,6 @@
 package controllers.dao;
 
+import models.CodecoolerModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -51,10 +52,48 @@ public class CodecoolerDAOTest {
     }
 
     @Test
-    public void getCodecoolerModelReturnsValidModel() {
+    public void getCodecoolerModelReturnsValidModel() throws SQLException{
         int codecoolerId = 1;
-        String codecoolerModelQuery = String.format("SELECT codecoolers.*, teams.id, login_access.email, login_access.password FROM codecoolers INNER JOIN" +
+        String sql = String.format("SELECT codecoolers.*, teams.id, login_access.email, login_access.password FROM codecoolers INNER JOIN" +
                 " teams ON codecoolers.codecooler_id = teams.codecooler_id INNER JOIN login_access ON codecoolers.codecooler_id = login_access.id WHERE" +
                 " codecoolers.codecooler_id = %d;", codecoolerId);
+        String password = "wTibie";
+        String email = "trzaskamWTibie@cc.com";
+        String nickName = "Cyklon";
+        String first_name = "Karol";
+        String second_name = "Trzaska";
+        int teamID = 1234;
+        int questInProgress = 1;
+        int coolCoinsEverEarned = 999;
+        int room = 3;
+        int expLevel = 10;
+        int coolcoins = 99;
+        when(statement.executeQuery(sql)).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, false);
+        when(resultSet.getInt("coolcoins")).thenReturn(coolcoins);
+        when(resultSet.getInt("exp_level")).thenReturn(expLevel);
+        when(resultSet.getInt("actual_room")).thenReturn(room);
+        when(resultSet.getInt("coolcoins_ever_earned")).thenReturn(coolCoinsEverEarned);
+        when(resultSet.getInt("quest_in_progress")).thenReturn(questInProgress);
+        when(resultSet.getString("first_name")).thenReturn(first_name);
+        when(resultSet.getString("last_name")).thenReturn(second_name);
+        when(resultSet.getString("nickname")).thenReturn(nickName);
+        when(resultSet.getInt("id")).thenReturn(teamID);
+        when(resultSet.getString("email")).thenReturn(email);
+        when(resultSet.getString("password")).thenReturn(password);
+
+        CodecoolerModel model = (new CodecoolerDAO(connection)).getCodecoolerModel(codecoolerId);
+
+        assertEquals(password, model.getPassword());
+        assertEquals(email, model.getEmail());
+        assertEquals(nickName, model.getNickname());
+        assertEquals(first_name, model.getFirstName());
+        assertEquals(second_name, model.getLastName());
+        assertEquals(teamID, model.getTeamID());
+        assertEquals(questInProgress, model.getQuestInProgress());
+        assertEquals(coolCoinsEverEarned, model.getCoolcoinsEverEarned());
+        assertEquals(room, model.getRoom());
+        assertEquals(expLevel, model.getExpLevel());
+        assertEquals(coolcoins, model.getCoolcoins());
     }
 }
